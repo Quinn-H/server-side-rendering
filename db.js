8b4511ee6402e331c1c86eeffa1aad271eed58b4
Quinn-H <quinn.hu618@gmail.com>
2016-12-06 11:24:41 +1300

    accepted Id input

--- db.js
+++ db.js
@@ -1,20 +1,30 @@
 module.exports = {
-  getHomeData: getHomeData
+  // getHomeData: getHomeData,
+  getName: getName
 }
 
-function getHomeData () {
-  return {
-    title: 'Awesome title',
-    headerText: 'Welcome to Awesome',
-    fruitList: [{
-      name: 'apples',
-      colour: 'red'
-    }, {
-      name: 'oranges',
-      colour: 'orange'
-    }, {
-      name: 'bananas',
-      colour: 'yellow'
-    }]
+var repos = require('./github')
+
+// function getHomeData () {
+//   return {
+//     title: 'Awesome title',
+//     headerText: 'Welcome to Awesome',
+//     fruitList: [{
+//       name: 'apples',
+//       colour: 'red'
+//     }, {
+//       name: 'oranges',
+//       colour: 'orange'
+//     }, {
+//       name: 'bananas',
+//       colour: 'yellow'
+//     }]
+//   }
+// }
+
+function getName () {
+  var data = {
+    repos: repos
   }
+  return data
 }
--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 var db = require('./db')
 
 module.exports = {
-  home: home
+  // home: home,
+  getReposName: getReposName
 }
 
-function home (req, res) {
-  var data = db.getHomeData()
-  data.showList = req.query.showlist
+// function home (req, res) {
+//   var data = db.getHomeData()
+//   data.showList = req.query.showlist
+//   res.render('home', data)
+// }
+
+function getReposName (req, res) {
+  var data = db.getName()
   res.render('home', data)
 }
